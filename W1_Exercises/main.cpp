0a6c3ded0e6c62b0bd7a62a548addb08a8922275
Jameson Hoang <hoangjam@sheridancollege.ca>
2021-01-20 20:09:57 -0500

    cnofusing

--- W1_Exercises/main.cpp
+++ W1_Exercises/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    // prints Hello 10x
+    /*for (int i = 0; i < 10; i++) {
+        cout << "Hello" << endl;
+    }*/
+
+    // prints *'s in a pattern top heavy pyramid
+    for (int i = 10; i > 0; i -= 2) {
+        for (int j = 0; j < i; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
+    // prints pyramid right justified 1 on top
+    for (int i = 0; i < 10; i++) {
+        cout << i;
+        for (int j = i; j < 10; j++) {
+            cout << " ";
+        }
+        for (int j = 0; j < i; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
     return 0;
 }
