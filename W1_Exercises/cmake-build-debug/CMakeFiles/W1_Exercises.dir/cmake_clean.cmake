57312998f2568155d66ecd26680be883aacd66c0
Jameson Hoang <hoangjam@sheridancollege.ca>
2021-01-20 19:11:24 -0500

    yuh

---
+++ W1_Exercises/cmake-build-debug/CMakeFiles/W1_Exercises.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/W1_Exercises.dir/main.cpp.o"
+  "W1_Exercises.exe"
+  "W1_Exercises.pdb"
+  "libW1_Exercises.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/W1_Exercises.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ W1_Exercises/cmake-build-debug/CMakeFiles/W1_Exercises.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for W1_Exercises.
+# This may be replaced when dependencies are built.
